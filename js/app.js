eba502359b8a5b58f9897ee46a5bdafc7c20a450
Erika Joy De La Cruz <erikajoy.odc@gmail.com>
2021-02-04 13:13:54 -0800

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
-const baguetteBoxMin = require("./baguetteBox.min");
 
-baguetteBoxMin.run('.gallery');
+
+baguetteBox.run('.gallery');
+const search = new Filter('search', 'data-caption');
+const baguetteBoxMin = require("./baguetteBox.min");
